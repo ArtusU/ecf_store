c76f909f72ddf6d2b310be512549a07b8775316e
ArtusU <artusrock@hotmail.com>
2021-04-04 12:13:19 +0100

    products list view

--- cart/models.py
+++ cart/models.py
@@ -1,9 +1,7 @@
 from django.contrib.auth import get_user_model
-
 from django.db import models
-from django.db.models.base import ModelState
-from django.utils.translation import activate
-
+from django.db.models.signals import pre_save
+from django.utils.text import slugify
 
 User = get_user_model()
 
@@ -30,8 +28,10 @@ class Address(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=200)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
+    image = models.ImageField(upload_to='product_images')
     description = models.TextField()
+    price = models.IntegerField(default=0)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=False)
@@ -83,3 +83,11 @@ class Payment(models.Model):
     @property
     def reference_number(self):
         return f"PAYMENT-{self.order}-{self.pk}-{self.time_stamp}"
+
+
+def pre_save_product_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = slugify(instance.title)
+
+pre_save.connect(pre_save_product_receiver, sender=Product)
+
--- cart/urls.py
+++ cart/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from . import views
 
 app_name = 'cart'
 
 urlpatterns = [
-    #path('', views.HomeView.as_view(), name='home'),
+    path('', views.ProductListView.as_view(), name='product-list'),
 ]
