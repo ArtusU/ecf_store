c5e65bedf47842271f262c490285e02b8ea6c16e
ArtusU <artusrock@hotmail.com>
2021-04-15 09:47:32 +0100

    staff:product-list and product-delete

--- staff/urls.py
+++ staff/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'staff'
 
 urlpatterns = [
-    path('', views.StaffViews.as_view(), name='staff')
+    path('', views.StaffViews.as_view(), name='staff'),
+    path('product-list/', views.ProductListView.as_view(), name='product-list'),
+    path('product/<pk>/delete/', views.ProductDeleteView.as_view(), name='product-delete')
 ]
